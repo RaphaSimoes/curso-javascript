5f9e26b022624b81ebb98867b3e7d9903b050b44
RaphaSimoes <you@example.com>
2025-09-08 09:25:09 -0300

    aulas 1,2,3,4

---
+++ index.js
@@ -0,0 +1,2 @@
+// O console.log() pode exibir múltiplos valores de uma só vez, como strings e números. Basta separá-los por vírgula.
+console.log("Meu nome é 'Raphaela' e eu estou aprendendo JavaScript às", 10, "da manhã");
