81ac60102165b84c96419f00e16cd8a17ba641f6
RaphaSimoes <you@example.com>
2025-09-08 10:30:59 -0300

    Aprendi sobre const, e suas regras e boas maneiras, e quando devemos usar let ou const

--- js/index.js
+++ js/index.js
@@ -1,19 +1,7 @@
-//criando variáveis com LET
-// A vírgula serve para exibir o valor de uma variável junto com outros textos, inserindo um espaço automático entre eles.
-let nome = "Raphaela";
-console.log(nome,"nasceu em 2003.")
-console.log("Quando",nome,"tinha 5 anos começou a usar óculos.")
-console.log("Mãe de",nome,"decidiu cortar seu cabelo.")
-console.log("O corte do cabelo de",nome,"era tigelinha e estava torto.")
-console.log(nome, "usava óculos e tinha um cabelo de tigelinha torto, cruzes.")
+//criando Const 
+const primeiroNumero = 20
+const segundoNumero = 25 
 
+const resultado = primeiroNumero + segundoNumero
 
-//IMPORTANTE ⚠️
-
-//Não podemos criar variáveis com palavras reservadas
-//Variáveis precisam ter nomes significativos 
-// Não pode começar o nome de uma variável com um número
-// Não podem conter espaços ou traços
-//Utilizamos camelCases 
-// Case-sensitive
-// Não podemos redeclarar variáveis com let
+console.log(resultado)
