54e1d18c2b43a963a9647c157b68a85b84098bff
RaphaSimoes <you@example.com>
2025-09-08 09:59:38 -0300

    Aprendi sobre a variável let, e suas regras e boas maneiras

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-alert("olá mundo");
+//criando variáveis com LET
+// A vírgula serve para exibir o valor de uma variável junto com outros textos, inserindo um espaço automático entre eles.
+let nome = "Raphaela";
+console.log(nome,"nasceu em 2003.")
+console.log("Quando",nome,"tinha 5 anos começou a usar óculos.")
+console.log("Mãe de",nome,"decidiu cortar seu cabelo.")
+console.log("O corte do cabelo de",nome,"era tigelinha e estava torto.")
+console.log(nome, "usava óculos e tinha um cabelo de tigelinha torto, cruzes.")
+
+
+//IMPORTANTE ⚠️
+
+//Não podemos criar variáveis com palavras reservadas
+//Variáveis precisam ter nomes significativos 
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+//Utilizamos camelCases 
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
