e6dc786f22b93a978ac16543eb7e9eecf0d995c2
RaphaSimoes <you@example.com>
2025-09-09 09:30:03 -0300

    Aula 8, aprendi a usar o tamplete string para melhorar a legibilidade e organização do meu código

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-//criando Const 
-const primeiroNumero = 20
-const segundoNumero = 25 
+// Exercicio Aula 8 
 
-const resultado = primeiroNumero + segundoNumero
+// Raphaela Cupertino Simões tem 22 anos, pesa 70kg
+// tem 1.76 de altura e seu IMC é de ... e ele nasceu em ...
 
-console.log(resultado)
+const nome = "Raphela"
+const sobrenome = "Cupertino Simões"
+const idade = 22
+const peso = 70
+const alturaEmM = 1.75
+const imc= peso / (alturaEmM * alturaEmM)
+let anoAtual = 2035
+const anoDeNascimento = anoAtual - idade
+
+console.log(nome, sobrenome,"tem", idade,"anos, pesa", peso,"kg, sua altura é de", alturaEmM, "e seu imc é", imc, " e ela nasceu em", anoDeNascimento)
+
+console.log()
+
+//Templete strings
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}kg sua altura é de ${alturaEmM} e seu imc é ${imc}, e ela nasceu em ${anoDeNascimento}`)
+
